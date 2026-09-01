208048e407f7c7860514c1c8ec4df1bb8e7a4871
samuzdev <deyverabanto@gmail.com>
2026-09-01 11:28:04 -0500

    feat: Use new calculator functions and hello.farewell in main

--- main.lua
+++ main.lua
@@ -4,8 +4,11 @@ local calculator = require("calculator")
 
 print("=== Lua Learning Project ===")
 print(hello.greet("World"))
+print(hello.farewell("Git"))
 print("Sum: " .. calculator.add(5, 3))
 print("Multiply: " .. calculator.multiply(4, 6))
+print("Power: " .. calculator.power(2, 8))
+print("Modulo: " .. calculator.modulo(17, 5))
 
 -- Practice: Feature branch will add more functions
 -- Practice: Hotfix branch will fix a bug
